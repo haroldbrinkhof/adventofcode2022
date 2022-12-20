0bdab5b70a3aee78ee3ec4e0687d66a9bf65739d
Harold Brinkhof <harold.brinkhof@gmail.com>
2022-12-20 01:57:08 +0100

    day 9 - part 1 revised

--- lisp/day9.lisp
+++ lisp/day9.lisp
@@ -17,35 +17,33 @@
         (loop for line = (read-line stream nil)
             while line collect line)))
 
-(defconstant +rows+ 5)
-(defconstant +row-start+ 0)
-(defconstant +row-end+ (- +rows+ 1))
-(defconstant +columns+ 6)
-(defconstant +col-start+ 0)
-(defconstant +col-end+ (- +columns+ 1))
-(defconstant +starting-position+ (* +row-end+ +columns+))
-
-(defstruct board
-    (head +starting-position+ :type integer)
-    (tail +starting-position+ :type integer))
-
 (defstruct grid-coordinate
   (row 0 :type integer)
   (column 0 :type integer))
 
+(defconstant +starting-position+ (make-grid-coordinate :row 0 :column 0))
+
+(defstruct board
+    (head +starting-position+ :type grid-coordinate)
+    (tail +starting-position+ :type grid-coordinate))
+
 (defun create-board-sequence (board command repetition)
+  "determine which action to perform and perform it REPETITION times,
+   returning list of sequential positions taken on the board"
   (let ((sequence))
   (dotimes (_ repetition)
-    (push (cond ((string= command "D") (move-head-down board))
-           ((string= command "L") (move-head-left board))
-           ((string= command "R") (move-head-right board))
-           ((string= command "U") (move-head-up board))
+    (push (cond ((string= command "D") (perform-action #'move-down board))
+           ((string= command "L") (perform-action #'move-left board))
+           ((string= command "R") (perform-action #'move-right board))
+           ((string= command "U") (perform-action #'move-up board))
            ('otherwise nil))
         sequence)
     (setf board (first sequence)))
   sequence))
 
-(defun move-head (board action)
+(defun perform-action (action board)
+  "move the head and move the head subsequently afterwards,
+   return a new board with these positions"
   (let* ((current-head (board-head board))
          (current-tail (board-tail board))
          (new-head-position (funcall action current-head))
@@ -54,112 +52,88 @@
                                     current-head)))
          (make-board :head new-head-position :tail new-tail-position)))
 
-(defun move-head-up (board)
-  (move-head board #'move-up))
-
-(defun move-head-down (board)
-  (move-head board #'move-down))
-(defun move-head-left (board)
-  (move-head board #'move-left))
-(defun move-head-right (board)
-  (move-head board #'move-right))
-
-(defun position-to-grid-coordinates (position)
-  "transform a position to row, column coordinates"
-  (multiple-value-bind (row column) (floor position +columns+)
-    (make-grid-coordinate :row row :column column)))
-(defun grid-coordinates-to-position (grid-coordinate)
-  "transform row, column coordinates to a position"
-  (+ (* +columns+ (grid-coordinate-row grid-coordinate)) (grid-coordinate-column grid-coordinate)))
-
 (defun move-up (starting-position)
   "calculates and returns the new position after moving up 1"
-  (if (> +columns+ starting-position)
-      starting-position
-      (- starting-position +columns+)))
+  (make-grid-coordinate :row (- (grid-coordinate-row starting-position) 1) :column (grid-coordinate-column starting-position)))
 
 (test move-up-1-row
-  (is-true (= 2 (move-up 8))))
-
-(test move-up-1-row-when-on-top-row-same-position-returned
-  (is-true (= 2 (move-up 2))))
+  (is-true (grid-coordinate= (make-grid-coordinate :row 0 :column 0) (move-up (make-grid-coordinate :row 1 :column 0)))))
 
 (defun move-down (starting-position)
   "calculates and returns the new position after moving down 1"
-  (if (<= (* +row-end+ +columns+ ) starting-position)
-      starting-position
-      (+ starting-position +columns+)))
+  (make-grid-coordinate :row (1+ (grid-coordinate-row starting-position)) :column (grid-coordinate-column starting-position)))
 
 (test move-down-1-row
-  (is-true (= 14 (move-down 8))))
-
-(test move-down-1-row-when-on-bottom-row-same-position-returned
-  (is-true (= 28 (move-down 28))))
+  (is-true (grid-coordinate= (make-grid-coordinate :row 1 :column 0) (move-down (make-grid-coordinate :row 0 :column 0)))))
 
 (defun move-left (starting-position)
-  (if (= starting-position 0)
-      starting-position
-      (- starting-position 1)))
+  "calculates and returns the new position after moving left 1"
+  (make-grid-coordinate :row (grid-coordinate-row starting-position) :column (- (grid-coordinate-column starting-position) 1)))
 
 (test move-left-1-row
-  (is-true (= 7 (move-left 8))))
-
-(test move-left-1-row-when-on-top-left-same-position-returned
-  (is-true (= 0 (move-left 0))))
+  (is-true (grid-coordinate= (make-grid-coordinate :row 0 :column 6) (move-left (make-grid-coordinate :row 0 :column 7)))))
 
 (defun move-right (starting-position)
-  (if (= (1+ starting-position) (* +rows+ +columns+))
-      starting-position
-      (1+ starting-position)))
+  "calculates and returns the new position after moving right 1"
+  (make-grid-coordinate :row (grid-coordinate-row starting-position) :column (1+ (grid-coordinate-column starting-position))))
 
 (test move-right-1-row
-  (is-true (= 9 (move-right 8))))
+  (is-true (grid-coordinate= (make-grid-coordinate :row 0 :column 1)  (move-right (make-grid-coordinate :row 0 :column 0)))))
 
-(test move-right-1-row-when-on-bottom-right-same-position-returned
-  (is-true (= 29 (move-right 29))))
+(defun grid-coordinate= (one two)
+  "predicate to determine equality between two grid-coordinate"
+  (and (= (grid-coordinate-row one) (grid-coordinate-row two))
+       (= (grid-coordinate-column one) (grid-coordinate-column two))))
+
+(test equality-of-grid-coordinates
+  (is-true (grid-coordinate= +starting-position+ +starting-position+))
+  (is-false (grid-coordinate= +starting-position+ (make-grid-coordinate :row 99 :column 0))))
 
 (defun positions-adjacent-p (position1 position2)
-  (or (= position1 position2)
-      (= position1 (move-left position2))
-      (= position1 (move-right position2))
-      (= position1 (move-up position2))
-      (= position1 (move-down position2))
-      (= position1 (move-up (move-left position2)))
-      (= position1 (move-up (move-right position2)))
-      (= position1 (move-down (move-left position2)))
-      (= position1 (move-down (move-right position2)))))
+  "predicate to determine if 2 positions are adjacent to each other"
+  (or (grid-coordinate= position1 position2)
+      (grid-coordinate= position1 (move-left position2))
+      (grid-coordinate= position1 (move-right position2))
+      (grid-coordinate= position1 (move-up position2))
+      (grid-coordinate= position1 (move-down position2))
+      (grid-coordinate= position1 (move-up (move-left position2)))
+      (grid-coordinate= position1 (move-up (move-right position2)))
+      (grid-coordinate= position1 (move-down (move-left position2)))
+      (grid-coordinate= position1 (move-down (move-right position2)))))
 
 (test positions-too-far-away-not-adjacent
-  (is-false (positions-adjacent-p 1 3)))
+  (is-false (positions-adjacent-p (make-grid-coordinate :row 0 :column 0) (make-grid-coordinate :row 1 :column 3))))
 
 (test identical-positions-count-as-adjacent
-  (is-true (positions-adjacent-p 1 1)))
+  (is-true (positions-adjacent-p +starting-position+ +starting-position+)))
 
 (test position-left-of-other-is-adjacent
-  (is-true (positions-adjacent-p 0 1)))
+  (is-true (positions-adjacent-p (make-grid-coordinate :row 0 :column 1) (make-grid-coordinate :row 0 :column 2))))
 
 (test position-right-of-other-is-adjacent
-  (is-true (positions-adjacent-p 2 1)))
+  (is-true (positions-adjacent-p (make-grid-coordinate :row 0 :column 2) (make-grid-coordinate :row 0 :column 1))))
 
 (test position-above-other-is-adjacent
-  (is-true (positions-adjacent-p 2 8)))
+  (is-true (positions-adjacent-p (make-grid-coordinate :row 1 :column 0) (make-grid-coordinate :row 0 :column 0))))
 
 (test position-below-other-is-adjacent
-  (is-true (positions-adjacent-p 7 1)))
+  (is-true (positions-adjacent-p (make-grid-coordinate :row 0 :column 0) (make-grid-coordinate :row 1 :column 0))))
 
 (test position-below-and-left-of-other-is-adjacent
-  (is-true (positions-adjacent-p 6 1)))
+  (is-true (positions-adjacent-p (make-grid-coordinate :row 1 :column 0) (make-grid-coordinate :row 0 :column 1))))
 
 (test position-below-and-right-of-other-is-adjacent
-  (is-true (positions-adjacent-p 8 1)))
+  (is-true (positions-adjacent-p (make-grid-coordinate :row 1 :column 2) (make-grid-coordinate :row 0 :column 1))))
 
 (test position-above-and-left-of-other-is-adjacent
-  (is-true (positions-adjacent-p 1 8)))
+  (is-true (positions-adjacent-p (make-grid-coordinate :row 0 :column 0) (make-grid-coordinate :row 1 :column 1))))
 
 (test position-above-and-right-of-other-is-adjacent
-  (is-true (positions-adjacent-p 3 8)))
+  (is-true (positions-adjacent-p (make-grid-coordinate :row 0 :column 2) (make-grid-coordinate :row 1 :column 1))))
 
-(defun solution1 (contents)
+
+(defun solution-to-part-1 (contents)
+  "returns the number of positions our tail takes"
     (let* ((current-board (make-board))
            (tail-positions-found)
            (boards (list current-board)))
@@ -171,16 +145,13 @@
             (push board boards)))
         (setf current-board (first board-sequence))))
     (dolist (board boards)
-      (unless (member (board-tail board) tail-positions-found)
+      (unless (member (board-tail board) tail-positions-found :test #'grid-coordinate=)
         (push (board-tail board) tail-positions-found)))
     (length tail-positions-found)))
 
-(test solution1-verification-of-example-should-be-13
+(test solution-to-part-1-verification-of-example-should-be-13
   (let ((test-contents (list "R 4" "U 4" "L 3" "D 1" "R 4" "D 1" "L 5" "R 2")))
     (is-true (= 13 (solution1 test-contents)))))
 
 
-;;; test works perfectly for provided example, values don't match for what's expected with input data
-;;; constraints unclear, no data concerning 'field', giving up, too frustrated with lack of correct info on constraints.
-;;; maybe pick up again later
-(format t "solution to part 1: ~a~%" (solution1 (get-contents)))
+(format t "solution to part 1: ~a~%" (solution-to-part-1 (get-contents)))
